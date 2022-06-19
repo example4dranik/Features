ea27e3dd7e1ff15b1f3c39f98107e7bebacefb37
dranik <dranik111100rs@gmail.com>
2022-06-19 23:02:33 +0300

    Added ImageMedia

---
+++ SimpleFeatures/ImageMedia/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ImageMedia
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string CurrentDirectory = Directory.GetCurrentDirectory();
+        private readonly string ScrDirectory = "files";
+        private readonly string TampleNumber = "#";
+        private readonly string TamplePng = "coding#.png";
+        private readonly string TampleGif = "coding#.gif";
+        private int _startIndex;
+        private int _currentIndex;
+        private int _lengthIndex;
+
+        public MainWindow()
+        {
+            _startIndex = 1;
+            _currentIndex = _startIndex;
+            _lengthIndex = 2;
+            InitializeComponent();
+        }
+
+        private void ButtonShowImage_Click(object sender, RoutedEventArgs e)
+        {
+            PreviewImage.Source = new BitmapImage(new Uri(GetPathToFile(TamplePng.Replace(TampleNumber, GetCurrentIndex().ToString()))));
+        }
+
+        private void ButtonShowMedia_Click(object sender, RoutedEventArgs e)
+        {
+            var media = new MediaElement();
+            media.Source = new Uri(GetPathToFile(TampleGif.Replace(TampleNumber, GetCurrentIndex().ToString())));
+            PreviewMedia.Source = media.Source;
+        }
+
+        private string GetPathToFile(string fileName) => System.IO.Path.Combine(CurrentDirectory, ScrDirectory, fileName);
+
+        private int GetCurrentIndex()
+        {
+            var current = _currentIndex;
+
+            _currentIndex = (_currentIndex + 1 <= _lengthIndex) ? _currentIndex + 1 : _startIndex;
+
+            return current;
+        }
+    }
+}
